498700ff238f10e0e147826c71314c51020b66f0
shunf4 <shun1048576@gmail.com>
2020-02-15 02:51:56 +0800

    fix: dist.sh on x86

--- cygwin-build/dist.sh
+++ cygwin-build/dist.sh
@@ -28,6 +28,6 @@ cmd /c dist.bat
 zip -j ../../proxychains_"$PXCH_VERSION"_win32_x64d.zip ../COPYING ../README*.md ../proxychains.conf ../x64/Debug/proxychains_x64d.exe ../x64/Debug/proxychains_hook_x64d.dll
 zip -j ../../proxychains_"$PXCH_VERSION"_win32_x64.zip ../COPYING ../README*.md ../proxychains.conf ../x64/Release/proxychains_x64.exe ../x64/Release/proxychains_hook_x64.dll
 zip -j ../../proxychains_"$PXCH_VERSION"_win32_x86d.zip ../COPYING ../README*.md ../proxychains.conf ../Debug/proxychains_x86d.exe ../Debug/proxychains_hook_x86d.dll
-zip -j ../../proxychains_"$PXCH_VERSION"_win32_x86.zip ../COPYING ../README*.md ../proxychains.conf ../Release/proxychains_x86.exe ../Release/proxychains_hook_x86d.dll
+zip -j ../../proxychains_"$PXCH_VERSION"_win32_x86.zip ../COPYING ../README*.md ../proxychains.conf ../Release/proxychains_x86.exe ../Release/proxychains_hook_x86.dll
 make release
 zip -j ../../proxychains_"$PXCH_VERSION"_cygwin_x64.zip ../COPYING ../README*.md ../proxychains.conf proxychains_x64.exe cygproxychains_hook_x64.dll
